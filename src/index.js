e5a7253f609db4c42ff4768c1a05128b57130ebb
Semyon Pisarev <cema.rus@gmail.com>
2017-10-31 16:46:42 -0700

    Install Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
+
+import './styles/index.css';
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj9g4tepn5fau0129i832ku8j',
+});
+
+const client = new ApolloClient({ networkInterface });
+
+ReactDOM.render(
+  <ApolloProvider client={ client }>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
